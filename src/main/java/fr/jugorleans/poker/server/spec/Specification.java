25f30f11b5be0df4e4e2e5ff5f6bd8d75c19c93d
Steve Cancès <steve.cances@gmail.com>
2015-02-23 23:29:31 +0100

    Correctif Specification basée sur Predicate

--- src/main/java/fr/jugorleans/poker/server/spec/Specification.java
+++ src/main/java/fr/jugorleans/poker/server/spec/Specification.java
@@ -10,19 +10,44 @@ import java.util.function.Predicate;
  */
 public interface Specification<T> extends Predicate<T> {
 
-    /**
-     * Check if {@code t} is satisfied by the {@link Specification}.
-     *
-     * @param t Object to test.
-     * @return {@code true} if {@code t} satisfies the specification.
-     */
-    boolean isSatisfiedBy(T t);
+	/**
+	 * Check if {@code t} is satisfied by the {@link Specification}.
+	 *
+	 * @param t Object to test.
+	 * @return {@code true} if {@code t} satisfies the specification.
+	 */
+	boolean isSatisfiedBy(T t);
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    default boolean test(final T t) {
-        return this.isSatisfiedBy(t);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	default boolean test(final T t) {
+		return this.isSatisfiedBy(t);
+	}
+
+	/**
+	 * @see Predicate#and(Predicate)
+	 */
+	default Specification<T> and(final Specification<? super T> other) {
+		final Predicate<T> and = Predicate.super.and(other);
+		return t -> and.test(t);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	default Specification<T> negate() {
+		final Predicate<T> negate = Predicate.super.negate();
+		return t -> negate.test(t);
+	}
+
+	/**
+	 * @see Predicate#or(Predicate)
+	 */
+	default Specification<T> or(final Specification<? super T> other) {
+		final Predicate<T> or = Predicate.super.or(other);
+		return t -> or.test(t);
+	}
 }
