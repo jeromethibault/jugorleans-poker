4aeef134f7d4deffa81b2b9895ce055c837f0337
Jérôme <jeromethibault89@gmail.com>
2015-02-28 21:58:36 +0100

    refactoring

--- src/main/java/fr/jugorleans/poker/server/game/StrongestHandResolver.java
+++ src/main/java/fr/jugorleans/poker/server/game/StrongestHandResolver.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * WinnigHandResolver interface.
  */
-public interface WinningHandResolver {
+public interface StrongestHandResolver {
 
     /**
      * Déterminer les mains gagnantes sur un board donné
