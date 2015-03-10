901d2eecf9af4bbb3662424c870f5f514dcabe8f
François Penaud <francoispenaud@gmail.com>
2015-03-10 18:55:26 +0100

    Ajout notion de Table

---
+++ src/main/java/fr/jugorleans/poker/server/tournament/Table.java
@@ -0,0 +1,18 @@
+package fr.jugorleans.poker.server.tournament;
+
+/**
+ * Table de jeu
+ */
+public class Table {
+
+    /**
+     * ID de la table
+     */
+    private String id;
+
+    /**
+     * Nom de la table
+     */
+    private String name;
+
+}
