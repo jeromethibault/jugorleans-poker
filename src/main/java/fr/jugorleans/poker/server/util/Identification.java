7cb30c56f7ab5f426fddb60ec16a562b019f52cd
François Penaud <francoispenaud@gmail.com>
2015-03-14 15:39:05 +0100

    Ajour classe utilitaire de gestion des différents identifiants
    idTournoi_numTable_numPlay

---
+++ src/main/java/fr/jugorleans/poker/server/util/Identification.java
@@ -0,0 +1,24 @@
+package fr.jugorleans.poker.server.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Classe utilitaire de manipulation des identifiants
+ */
+public final class Identification {
+
+    /**
+     * Separateur entre id tournament, id table et id play
+     */
+    public static final String ID_SEPARATOR = "_";
+
+    /**
+     * Récupération de l'idTable depuis l'idPlay
+     *
+     * @param idPlay identifiant d'un Play
+     * @return l'id de la table associée
+     */
+    public String getIdTable(String idPlay) {
+        return StringUtils.substringAfterLast(idPlay, ID_SEPARATOR);
+    }
+}
